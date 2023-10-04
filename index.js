c947a023d5f15a4d61cfa37ce7cdb579fcc8079d
GitAfrica <pmogale88@gmail.com>
2023-10-04 15:20:45 +0200

    added checkout button

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const addToCartButtons = document.querySelectorAll(".add-to-cart");
   const cartItemsList = document.getElementById("cart-items");
   const cartTotal = document.getElementById("cart-total");
+  const checkoutButton = document.getElementById("checkout-button");
 
-  let cart = [];
-  let total = 0;
+  let order = {
+    items: [],
+    total: 0,
+  };
+
+  function addToCart(name, price) {
+    order.items.push({ name, price });
+    order.total += price;
+    updateCart();
+  }
 
   function updateCart() {
     cartItemsList.innerHTML = "";
     total = 0;
 
-    cart.forEach((item) => {
+    order.items.forEach((item, index) => {
       const listItem = document.createElement("li");
       listItem.classList.add("cart-item");
       listItem.innerHTML = `
-                <span>${item.name}</span>
-                <span>R${item.price}</span>
-                <button class="remove-item" data-name="${item.name}">Remove</button>
-            `;
+            <span>${item.name}</span>
+            <span>R${item.price}</span>
+            <button class="remove-item" data-index="${index}">Remove</button>
+        `;
       cartItemsList.appendChild(listItem);
       total += item.price;
     });
@@ -32,19 +41,32 @@ document.addEventListener("DOMContentLoaded", function () {
       const price = parseFloat(
         product.querySelector("p").textContent.replace("Price: R", "")
       );
-      cart.push({ name, price });
-      updateCart();
+      addToCart(name, price);
     });
   });
 
   cartItemsList.addEventListener("click", function (event) {
     if (event.target.classList.contains("remove-item")) {
-      const itemName = event.target.getAttribute("data-name");
-      const itemIndex = cart.findIndex((item) => item.name === itemName);
-      if (itemIndex !== -1) {
-        cart.splice(itemIndex, 1);
+      const itemIndex = parseInt(event.target.getAttribute("data-index"), 10);
+      if (
+        !isNaN(itemIndex) &&
+        itemIndex >= 0 &&
+        itemIndex < order.items.length
+      ) {
+        order.items.splice(itemIndex, 1);
         updateCart();
       }
+    }
+  });
+
+  checkoutButton.addEventListener("click", function () {
+    if (order.items.length === 0) {
+      alert("Your cart is empty. Add items before checkout.");
+    } else {
+      alert("Thank you for your purchase!");
+      order.items = [];
+      order.total = 0;
+      updateCart();
     }
   });
 });
